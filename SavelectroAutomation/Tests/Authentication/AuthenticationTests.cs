217cf58bf2af714b1f262dc37ff601ed46aae999
Octavian80 <orosromum@gmail.com>
2022-03-19 15:12:16 +0200

    Creation of framework structure

---
+++ SavelectroAutomation/Tests/Authentication/AuthenticationTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using SavelectroAutomation.Tests;
+using SavelectroAutomation.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SavelectroAutomation.Tests.Authentication
+{
+    class AuthenticationTests:BaseTest
+    {
+
+        string url = FrameworkConstants.GetUrl();
+        [Test]
+
+        public void AuthenticationPositive()
+        {
+            testName = TestContext.CurrentContext.Test.Name;
+            _test = _extent.CreateTest(testName);
+            _driver.Navigate().GoToUrl(url);
+        }
+    }
+}
