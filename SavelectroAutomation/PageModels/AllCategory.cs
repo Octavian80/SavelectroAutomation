2e6b18b9cf2bd0c505e2bce119be157c578894bc
Octavian80 <orosromum@gmail.com>
2022-03-21 22:24:36 +0200

    CategoryMenu

---
+++ SavelectroAutomation/PageModels/AllCategory.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using SavelectroAutomation.PageModels.POM;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SavelectroAutomation.PageModels
+{
+    class AllCategory : BasePage
+    {
+        const string allCategorySelector = "mega_10_category";//id
+        const string categorySelector = "[id ^=mega_10_category__cat_]";//id
+        public AllCategory(IWebDriver driver) : base(driver)
+        {
+
+        }
+
+        public void SelectAllCategories()
+        {
+            var allCategory = driver.FindElement(By.Id(allCategorySelector));
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(allCategory).Build().Perform();
+            var category = driver.FindElements(By.CssSelector(categorySelector));
+            Console.WriteLine(category.Count);
+
+        }
+    }
+}
---
+++ SavelectroAutomation/Tests/CategoryMenu/CategoryMenuTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using SavelectroAutomation.PageModels;
+using SavelectroAutomation.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SavelectroAutomation.Tests.CategoryMenu
+{
+    class CategoryMenuTests:BaseTest
+    {
+        string url = FrameworkConstants.GetUrl();
+
+        [Test]
+
+        public void Move()
+        {
+            testName = TestContext.CurrentContext.Test.Name;
+            _test = _extent.CreateTest(testName);
+            _driver.Navigate().GoToUrl(url);
+            MainPage mp = new MainPage(_driver);
+            mp.AcceptCookies();
+            AllCategory cm = new AllCategory(_driver);
+            cm.SelectAllCategories();
+            
+
+        }
+    }
+}
