1a1c916a1f97556b0bd662b347e92ffff6cb0e7e
Octavian80 <orosromum@gmail.com>
2022-03-27 15:48:29 +0300

    Registration Test , Xpath dynamic Id

--- SavelectroAutomation/PageModels/RegistrationPage.cs
+++ SavelectroAutomation/PageModels/RegistrationPage.cs
@@ -1,4 +1,6 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using SavelectroAutomation.PageModels.POM;
 using System;
 using System.Collections.Generic;
@@ -23,6 +25,7 @@ namespace SavelectroAutomation.PageModels
         const string personCountrySelector = "profile_billing_country";//id
         const string personStateSelector = "profile_billing_state";//id
         const string personCitySelector = "profile_billing_city";//id
+        const string cityListSelector = "body > div:nth-child(20)";//css
         const string personAdressSelector = "profile_billing_address";//id
         const string personZipCodeSelector = "profile_billing_zip";//id
         const string personPhoneSelector = "profile_billing_phone";//id
@@ -38,6 +41,16 @@ namespace SavelectroAutomation.PageModels
         const string newsletterSelector = "#user_account_fields > div.row-newsletter-terms-submit > div.form-group.row-join-newsletter > div > div > label";//css
         const string termsConditionsSelector = "#user_account_fields > div.row-newsletter-terms-submit > div.form-group.row-confirm-terms > div > div > label";//css
         const string submitButtonSelector = "#user_account_fields > div.row-newsletter-terms-submit > button";//css
+        const string emailErrorSelector = "parsley-id-5";//id
+        const string passwordEmptyErrorSelector = "parsley-id-7";//id
+        const string passwordLenghtErrorSelector = "message_profile_password";//id
+        const string nameErrorSelector = "parsley-id-14";//id
+        const string cityErrorSelector = "parsley-id-22";//id
+        const string adressErrorSelector = "parsley-id-24";//id
+        const string phoneErrorSelector = "parsley-id-28";//id
+        const string companyNameErrorSelector = "//ul[starts-with(@id, 'parsley-id-')]";// XPath dynamic Id
+        const string companyPersonErrorSelector = "//ul[starts-with(@id, 'parsley-id-')]";// XPath dynamic Id
+
 
 
         public Boolean CheckRegistrationLabel(string label)
@@ -51,7 +64,7 @@ namespace SavelectroAutomation.PageModels
         {
         }
 
-        public void PersonRegistration( string email, string password,string name,string country,string city,string adress,string phone)
+        public void PersonRegistration( string email, string password,string name,string country,string adress,string phone)
         {
             var emailInput = driver.FindElement(By.Id(emailInputSelector));
             emailInput.Clear();
@@ -68,10 +81,10 @@ namespace SavelectroAutomation.PageModels
             countrySelect.Click();
             countrySelect.SendKeys(country);
             countrySelect.Click();
-            var citySelect = driver.FindElement(By.Id(personCitySelector));
+           /* var citySelect = driver.FindElement(By.Id(personCitySelector));            
             citySelect.Clear();
             citySelect.SendKeys(city);
-            citySelect.Click();
+            citySelect.Click();*/
             var personAdressInput = driver.FindElement(By.Id(personAdressSelector));
             personAdressInput.Clear();
             personAdressInput.SendKeys(adress);
@@ -90,7 +103,7 @@ namespace SavelectroAutomation.PageModels
 
         }
 
-        public void CompanyRegistration(string email, string password, string companyName,string tradeRegister,string vat,string person,string bank,string iban, string country, string city, string adress, string phone)
+        public void CompanyRegistration(string email, string password, string companyName,string tradeRegister,string vat,string person,string bank,string iban, string country, string adress, string phone)
         {
             var emailInput = driver.FindElement(By.Id(emailInputSelector));
             emailInput.Clear();
@@ -122,10 +135,14 @@ namespace SavelectroAutomation.PageModels
             countrySelect.Click();
             countrySelect.SendKeys(country);
             countrySelect.Click();
-            var citySelect = driver.FindElement(By.Id(personCitySelector));
-            citySelect.Clear();
-            citySelect.SendKeys(city);
-            citySelect.Click();
+            /*var personState= driver.FindElement(By.Id(personStateSelector));
+            personState.Click();
+            personState.SendKeys(state);
+            personState.Click();
+            var citySelect = driver.FindElement(By.Id(personCitySelector));                                
+            citySelect.SendKeys(Keys.ArrowDown+ city + Keys.Enter);
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+            executor.ExecuteScript("arguments[0].value='enter the value here';", citySelect); */          
             var personAdressInput = driver.FindElement(By.Id(personAdressSelector));
             personAdressInput.Clear();
             personAdressInput.SendKeys(adress);
@@ -143,5 +160,205 @@ namespace SavelectroAutomation.PageModels
 
 
         }
+
+        public void CompanySimplifiedRegistration(string email, string password, string companyName,string person,string country, string adress, string phone)
+        {
+            var emailInput = driver.FindElement(By.Id(emailInputSelector));
+            emailInput.Clear();
+            emailInput.SendKeys(email);
+            var passwordInput = driver.FindElement(By.Id(passwordInputSelector));
+            passwordInput.Clear();
+            passwordInput.SendKeys(password);
+            var companyButton = driver.FindElement(By.CssSelector(companyButtonSelector));
+            companyButton.Click();
+            var companyNameInput = driver.FindElement(By.Id(companyNameSelector));
+            companyNameInput.Clear();
+            companyNameInput.SendKeys(companyName);
+            /*var billingNumberInput = driver.FindElement(By.Id(billingNumberSelector));
+            billingNumberInput.Clear();
+            billingNumberInput.SendKeys(tradeRegister);
+            var vatIput = driver.FindElement(By.Id(vatNumberSelector));
+            vatIput.Clear();
+            vatIput.SendKeys(vat);*/
+            var companyPersonInput = driver.FindElement(By.Id(billingNameSelector));
+            companyPersonInput.Clear();
+            companyPersonInput.SendKeys(person);
+            /*var bankNameInput = driver.FindElement(By.Id(bankNameSelector));
+            bankNameInput.Clear();
+            bankNameInput.SendKeys(bank);
+            var ibanInput = driver.FindElement(By.Id(ibanSelector));
+            ibanInput.Clear();
+            ibanInput.SendKeys(iban);*/
+            var countrySelect = driver.FindElement(By.Id(personCountrySelector));
+            countrySelect.Click();
+            countrySelect.SendKeys(country);
+            countrySelect.Click();
+            /*var personState= driver.FindElement(By.Id(personStateSelector));
+            personState.Click();
+            personState.SendKeys(state);
+            personState.Click();
+            var citySelect = driver.FindElement(By.Id(personCitySelector));                                
+            citySelect.SendKeys(Keys.ArrowDown+ city + Keys.Enter);
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+            executor.ExecuteScript("arguments[0].value='enter the value here';", citySelect); */
+            var personAdressInput = driver.FindElement(By.Id(personAdressSelector));
+            personAdressInput.Clear();
+            personAdressInput.SendKeys(adress);
+            var personPhoneInput = driver.FindElement(By.Id(personPhoneSelector));
+            personPhoneInput.Clear();
+            personPhoneInput.SendKeys(phone);
+            var personShippingAdress = driver.FindElement(By.CssSelector(sameShippingAdressSelector));
+            personShippingAdress.Click();
+            var newsletter = driver.FindElement(By.CssSelector(newsletterSelector));
+            newsletter.Click();
+            var termsAndConditions = driver.FindElement(By.CssSelector(termsConditionsSelector));
+            termsAndConditions.Click();
+            var submitRegistration = driver.FindElement(By.CssSelector(submitButtonSelector));
+            submitRegistration.Submit();
+
+
+        }
+
+
+        public string EmailError()
+        {
+            var exists = Utils.FindElementSafe(driver, By.Id(emailErrorSelector));
+
+            if (exists == null) return "";
+
+            var emailError = driver.FindElement(By.Id(emailErrorSelector));
+            if (emailError.Displayed)
+                return emailError.Text;
+            return "";
+        }
+
+        public string PasswordEmptyError()
+        {
+
+            var exists = Utils.FindElementSafe(driver, By.Id(passwordEmptyErrorSelector));
+
+            if (exists == null) return "";
+
+            var passError = driver.FindElement(By.Id(passwordEmptyErrorSelector));
+            if (passError.Displayed)
+            {
+                return passError.Text;
+            }
+
+            return "";
+        }
+
+        public string PasswordLengthError()
+        {
+
+            var exists = Utils.FindElementSafe(driver, By.Id(passwordLenghtErrorSelector));
+
+            if (exists == null) return "";
+
+            var passError = driver.FindElement(By.Id(passwordLenghtErrorSelector));
+            if (passError.Displayed)
+            {
+                return passError.Text;
+            }
+
+            return "";
+        }
+
+        public string NameError()
+        {
+
+            var exists = Utils.FindElementSafe(driver, By.Id(nameErrorSelector));
+
+            if (exists == null) return "";
+
+            var passError = driver.FindElement(By.Id(nameErrorSelector));
+            if (passError.Displayed)
+            {
+                return passError.Text;
+            }
+
+            return "";
+        }
+
+        public string CityError()
+        {
+
+            var exists = Utils.FindElementSafe(driver, By.Id(cityErrorSelector));
+
+            if (exists == null) return "";
+
+            var passError = driver.FindElement(By.Id(cityErrorSelector));
+            if (passError.Displayed)
+            {
+                return passError.Text;
+            }
+
+            return "";
+        }
+
+        public string AdressError()
+        {
+
+            var exists = Utils.FindElementSafe(driver, By.Id(adressErrorSelector));
+
+            if (exists == null) return "";
+
+            var passError = driver.FindElement(By.Id(adressErrorSelector));
+            if (passError.Displayed)
+            {
+                return passError.Text;
+            }
+
+            return "";
+        }
+
+        public string PhoneError()
+        {
+
+            var exists = Utils.FindElementSafe(driver, By.Id(phoneErrorSelector));
+
+            if (exists == null) return "";
+
+            var passError = driver.FindElement(By.Id(phoneErrorSelector));
+            if (passError.Displayed)
+            {
+                return passError.Text;
+            }
+
+            return "";
+        }
+
+        public string CompanyNameError()
+        {
+
+            var exists = Utils.FindElementSafe(driver, By.XPath(companyNameErrorSelector));
+
+            if (exists == null) return "";
+
+            var passError = driver.FindElement(By.XPath(companyNameErrorSelector));
+            if (passError.Displayed)
+            {
+                return passError.Text;
+            }
+
+            return "";
+        }
+
+        public string CompanyPersonError()
+        {
+
+            var exists = Utils.FindElementSafe(driver, By.XPath(companyPersonErrorSelector));
+
+            if (exists == null) return "";
+
+            var passError = driver.FindElement(By.XPath(companyPersonErrorSelector));
+            if (passError.Displayed)
+            {
+                return passError.Text;
+            }
+
+            return "";
+        }
+
     }
 }
--- SavelectroAutomation/Tests/Register/RegistrationTests.cs
+++ SavelectroAutomation/Tests/Register/RegistrationTests.cs
@@ -10,9 +10,20 @@ namespace SavelectroAutomation.Tests.Register
     class RegistrationTests:BaseTest
     {
         string url = FrameworkConstants.GetUrl();
+
+        private static IEnumerable<TestCaseData> GetCredentialsDataCsv()
+        {
+            foreach (var values in Utils.GetGenericData("TestData\\personRegistrationData.csv"))
+            {
+                yield return new TestCaseData(values);
+            }
+        }
+
+      
+
         [Test]
 
-        public void PersonRegisterTest()
+        public void PersonRegisterPositiveTest()
         {
             testName = TestContext.CurrentContext.Test.Name;
             _test = _extent.CreateTest(testName);
@@ -22,7 +33,7 @@ namespace SavelectroAutomation.Tests.Register
             _driver.Navigate().GoToUrl(url + registrationUrlPath);
             RegistrationPage rp = new RegistrationPage(_driver);
             Assert.IsTrue(rp.CheckRegistrationLabel("Inregistrare"));
-            rp.PersonRegistration("aaaa@go.ro", "spartcus", "Georgel", "Romania", "Constanta", "Lacramioarei", "5555555555");
+            rp.PersonRegistration("aaaa@go.ro", "spartcus", "Georgel", "Romania", "Lacramioarei", "5555555555");
 
         }
         [Test]
@@ -36,8 +47,74 @@ namespace SavelectroAutomation.Tests.Register
             _driver.Navigate().GoToUrl(url + registrationUrlPath);
             RegistrationPage rp = new RegistrationPage(_driver);
             Assert.IsTrue(rp.CheckRegistrationLabel("Inregistrare"));
-            rp.CompanyRegistration("steelmetal@steel.ro","sssoososoo","Steel","J30/14/14","111111111","Onica Dorel","Alpha","11111111", "Romania", "Constanta", "Lacramioarei", "5555555555");
+            rp.CompanyRegistration("steelmetal@steel.ro", "sssoososoo", "Steel", "J30/14/14", "111111111", "Onica Dorel", "Alpha", "11111111", "Romania","Lacramioarei", "5555555555") ;
 
         }
+
+        /* [TestCase("aa@ro","123456GGG","AAA", "Romania","Stea","0744444444", "Valoarea introdusa trebuie sa fie o adresa de email valida.","","","","","")]
+         [TestCase("","123456GGG","AAA", "Romania","Stea","0744444444", "Aceasta valoare este obligatorie.","", "","","","")]
+         [TestCase("aa@ro.ro","123456GGG","AAA", "Romania","Stea","0744","","", "","","", "Valoarea introdusa nu este valida.")]
+         [TestCase("aa@ro.ro","123","AAA","Romania","Stea", "0744444444","","", "Parola trebuie sa contina litere si cifre si trebuie sa aiba minim 6 caractere.", "", "","")]
+         [TestCase("aa@ro.ro", "123456GGG","","Romania","Stea", "0744444444","","","", "Aceasta valoare este obligatorie.", "","")]
+         [TestCase("aa@ro.ro","123456GGG", "AAA", "Romania","", "0744444444","","","","", "Aceasta valoare este obligatorie.", "")]
+         [TestCase("","", "", "Romania","","", "Aceasta valoare este obligatorie.", "Aceasta valoare este obligatorie.", "", "Aceasta valoare este obligatorie.", "Aceasta valoare este obligatorie.", "Aceasta valoare este obligatorie.")]*/
+
+        [Test, TestCaseSource("GetCredentialsDataCsv")]
+        public void PersonRegisterNegative(string email, string password, string name, string country,string adress, string phone,string emailError,string passEmptyError,string passLengthError,string nameError,string adressError,string telephoneError)
+        {
+            testName = TestContext.CurrentContext.Test.Name;
+            _test = _extent.CreateTest(testName);
+            _driver.Navigate().GoToUrl(url);
+            MainPage mp = new MainPage(_driver);
+            mp.AcceptCookies();
+            _driver.Navigate().GoToUrl(url + registrationUrlPath);
+            RegistrationPage rp = new RegistrationPage(_driver);
+            Assert.IsTrue(rp.CheckRegistrationLabel("Inregistrare"));
+            rp.PersonRegistration(email, password,name,country,adress,phone);
+            Assert.AreEqual(emailError, rp.EmailError());
+            Assert.AreEqual(passEmptyError, rp.PasswordEmptyError());
+            Assert.AreEqual(passLengthError, rp.PasswordLengthError());
+            Assert.AreEqual(nameError, rp.NameError());           
+            Assert.AreEqual(adressError, rp.AdressError());
+            Assert.AreEqual(telephoneError, rp.PhoneError());
+        }
+
+        [TestCase("aaaa@aa.ro","aaaaaa","","pppppp","Romania","aaaaa","074444444","","Aceasta valoare este obligatorie.","")]
+        
+        public void CompanyNameRegisterNegative(string email, string password, string companyName,string person,string country, string adress, string phone,string emailError, string companyNameError,string personError)
+        {
+            testName = TestContext.CurrentContext.Test.Name;
+            _test = _extent.CreateTest(testName);
+            _driver.Navigate().GoToUrl(url);
+            MainPage mp = new MainPage(_driver);
+            mp.AcceptCookies();
+            _driver.Navigate().GoToUrl(url + registrationUrlPath);
+            RegistrationPage rp = new RegistrationPage(_driver);
+            Assert.IsTrue(rp.CheckRegistrationLabel("Inregistrare"));
+            rp.CompanySimplifiedRegistration(email, password, companyName, person,country, adress, phone);
+            Assert.AreEqual(emailError, rp.EmailError());           
+            Assert.AreEqual(companyNameError, rp.CompanyNameError());                 
+
+        }
+
+        [TestCase("aaaa@aa.ro", "aaaaaa","Iron","", "Romania", "aaaaa", "074444444", "","","Aceasta valoare este obligatorie.")]
+
+        public void CompanyPersonRegisterNegative(string email, string password, string companyName, string person, string country, string adress, string phone, string emailError, string companyNameError, string personError)
+        {
+            testName = TestContext.CurrentContext.Test.Name;
+            _test = _extent.CreateTest(testName);
+            _driver.Navigate().GoToUrl(url);
+            MainPage mp = new MainPage(_driver);
+            mp.AcceptCookies();
+            _driver.Navigate().GoToUrl(url + registrationUrlPath);
+            RegistrationPage rp = new RegistrationPage(_driver);
+            Assert.IsTrue(rp.CheckRegistrationLabel("Inregistrare"));
+            rp.CompanySimplifiedRegistration(email, password, companyName, person, country, adress, phone);
+            Assert.AreEqual(emailError, rp.EmailError());
+            Assert.AreEqual(personError, rp.CompanyPersonError());
+
+        }
+
+
     }
 }
