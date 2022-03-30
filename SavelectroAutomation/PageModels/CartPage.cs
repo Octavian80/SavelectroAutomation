19fcd01fd33a263b6d1ba91aa71e4c0a82343d86
Octavian80 <orosromum@gmail.com>
2022-03-30 20:31:19 +0300

    Emptyng cart

--- SavelectroAutomation/PageModels/CartPage.cs
+++ SavelectroAutomation/PageModels/CartPage.cs
@@ -8,16 +8,33 @@ namespace SavelectroAutomation.PageModels
 {
     class CartPage : BasePage
     {
+        const string cartMenuButtonSelector = "on-off";//class
+        const string emptyCartLabel = "#page_content > div.page-content-center > div > h1";//css 
+        const string cartProductsSelector = "prod-title";//class
+        const string deleteProductsButtonsSelector = "#page_cart_form > div > table > tbody > tr > td.text-center.d-none.d-lg-table-cell.col-narrow > p:nth-child(2) > a";//class
 
-        const string cartLabel = "fancy-title";
         public CartPage(IWebDriver driver) : base(driver)
         {
             
 
         }
-         public Boolean CheckCartPage(string label)
+
+        public void EmptyCart()
         {
-            return String.Equals(label.ToLower(), Utils.WaitForFluentElement(driver, 10, By.ClassName(cartLabel)));
+            var cartMenuButton = driver.FindElement(By.ClassName(cartMenuButtonSelector));
+            cartMenuButton.Click();
+            var deleteProductsButton = driver.FindElements(By.CssSelector(deleteProductsButtonsSelector));
+            foreach (IWebElement deleteButton in deleteProductsButton)
+            {
+                deleteButton.Click();                
+            }
+        }
+
+
+
+         public Boolean CheckEmptyCart(string label)
+        {
+            return String.Equals(label.ToLower(), driver.FindElement(By.CssSelector(emptyCartLabel)).Text.ToLower());
         }
     }
 }
--- SavelectroAutomation/Tests/CartAdd/CartTests.cs
+++ SavelectroAutomation/Tests/CartAdd/CartTests.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using NUnit.Framework;
+using SavelectroAutomation.PageModels;
+using SavelectroAutomation.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SavelectroAutomation.Tests.CartAdd
 {
-    class CartTests
+    class CartTests:BaseTest
     {
+        string url = FrameworkConstants.GetUrl();
+
+        [Test]
+
+        public void EmptyngCart()
+        {
+            testName = TestContext.CurrentContext.Test.Name;
+            _test = _extent.CreateTest(testName);
+            _driver.Navigate().GoToUrl(url);
+            MainPage mp = new MainPage(_driver);
+            mp.AcceptCookies();
+            mp.ClickAccesAccount();
+            LoginPage lp = new LoginPage(_driver);
+            Assert.IsTrue(lp.CheckLoginPage("Ai deja un cont?"));
+            lp.Login("octavianautomation@yahoo.com","Automation");
+            AllCategory cm = new AllCategory(_driver);
+            cm.AddToCartOneProduct(12, 2);
+            CartPage cap = new CartPage(_driver);
+            cap.EmptyCart();
+            Assert.IsTrue(cap.CheckEmptyCart("Cosul tau de cumparaturi este gol"));
+        }
+
+
+
     }
 }
